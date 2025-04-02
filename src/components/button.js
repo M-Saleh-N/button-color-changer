76895a40bcc70277248b60b07d2cac55b6bc5827
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-02 10:23:17 +0300

    made changes

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import "../styles/button.css"
+
+function Button() {
+
+}
+return(
+    <div className="button">
+        <button>click me</button>
+        
+    </div>
+)
