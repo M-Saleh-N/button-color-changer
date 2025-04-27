93092cf82b2caa6556a4059ec6e8b4ae59263ddc
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-27 11:56:03 +0300

    added condition

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,10 @@ import React, {useState} from "react";
 import "../styles/button.css"
 
 function Button() {
+    const [clicked, setClicked] = useState(false);
 
+    const handleClick = () => {
+      setClicked(!clicked);
 }
 return(
     <div className="button">
@@ -10,3 +13,4 @@ return(
         
     </div>
 )
+}
